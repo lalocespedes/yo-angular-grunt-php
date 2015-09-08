7f344a3274328431743a4f16a0263882768f32d9
user <user@debian-lenovo>
2015-09-08 12:15:37 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name youtubeApp
+ * @description
+ * # youtubeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('youtubeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youtubeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the youtubeApp
+ */
+angular.module('youtubeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    console.log("hola");
+
+  });
